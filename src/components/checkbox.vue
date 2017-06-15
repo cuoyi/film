ef67e646c374dd35851e8f2b44977710f3ce9127
pumpkin <356082079@qq.com>
2017-06-15 23:50:22 +0800

    checkbox

---
+++ src/components/checkbox.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <input type='checkbox' v-model='checked' @click='checkedAll'>全选
+    <template v-for='checkb in checkboxData'>
+      <input type='checkbox' v-model="checkb.ischeck" value='checkb.id' @click="checkOne">{{checkb.value}}
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'app',
+      checkboxData: [{
+        id: '1',
+        ischeck: false,
+        value: '苹果',
+      }, {
+        id: '2',
+        ischeck: false,
+        value: '荔枝'
+      }, {
+        id: '3',
+        ischeck: false,
+        value: '香蕉'
+      }, {
+        id: '4',
+        ischeck: false,
+        value: '火龙果'
+      }],
+      checked: false,
+      selects: []
+    }
+  },
+  methods: {
+    checkOne: function (item) {
+      let self = this;
+      item.ischeck = !item.ischeck;
+      this.checkboxData.some(function (item2) {
+        if (!item2.ischeck) {
+          self.checked = false;
+          return true;
+        }
+        self.checked = true;
+      });
+    },
+    checkedAll: function () {
+      let self = this;
+      this.checkboxData.forEach(function (item) {
+        if (!self.checked) {
+          item.ischeck = false;
+        }
+        else {
+          item.ischeck = true;
+        }
+      });
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/index'
 import Detail from '@/components/detail'
+import checkbox from '@/components/checkbox'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
             path: '/Detail',
             name: 'Detail',
             component: Detail
+        },
+        {
+            path: '/checkbox',
+            name: 'checkbox',
+            component: checkbox
         }
     ]
 })
